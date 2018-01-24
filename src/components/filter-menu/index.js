f014930168d2454e83165b2b4fd6f053211a34f7
Jose <jrleon90@gmail.com>
2018-01-23 21:34:35 -0500

    filter error

--- src/components/filter-menu/index.js
+++ src/components/filter-menu/index.js
@@ -4,12 +4,8 @@ import {Link} from 'react-router';
 class FilterMenu extends Component {
     render(){
         return(
-        <div className='well'>
-            <div className='list-group'>
-                <Link className='btn btn-default' to={}>
-
-                </Link>
-            </div>
+        <div>
+            Filter
         </div>
         );
     }
